c762078a8e48f7146a86ef373e05f93e01412f61
MiltonCoronado <miltonext@hotmail.com>
2024-11-30 13:47:56 -0500

    Layout component was implemted

--- src/router/App.jsx
+++ src/router/App.jsx
@@ -6,17 +6,23 @@ import MyOrders from '../pages/MyOrders';
 import Signin from '../pages/Signin';
 import NotFound from '../pages/NotFound';
 
+import NavBar from '../components/NavBar';
+import Layout from '../components/Layout';
+
 const App = () => {
   return (
     <HashRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/my-account' element={<MyAccount />} />
-        <Route path='/my-order' element={<MyOrder />} />
-        <Route path='/my-orders' element={<MyOrders />} />
-        <Route path='/sign-in' element={<Signin />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
+      <NavBar />
+      <Layout>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/my-account' element={<MyAccount />} />
+          <Route path='/my-order' element={<MyOrder />} />
+          <Route path='/my-orders' element={<MyOrders />} />
+          <Route path='/sign-in' element={<Signin />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Layout>
     </HashRouter>
   )
 };
