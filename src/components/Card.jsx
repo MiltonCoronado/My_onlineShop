8e3a421a8ead181bddcf278fbac8984c6b25a746
MiltonCoronado <miltonext@hotmail.com>
2024-12-01 11:22:31 -0500

    feat/function to send product data added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,14 +4,25 @@ import { MyContext } from './Context.jsx';
 const Card = ({ data }) => {
   const context = MyContext();
 
+  const handleSideMenu = () => {
+    context.openSideMenu();
+    context.setShowProduct(data);
+  };
+
   return (
-    <article className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
+    <article 
+      className='bg-white cursor-pointer w-56 h-60 rounded-lg'
+      onClick={() => handleSideMenu()}
+    >
       <figure className='relative mb-2 w-full h-4/5'>
         <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0-5'>{data.category}</span>
         <img className='w-full h-full object-cover rounded-lg' src={data.image} alt='headphones'/>
         <button 
           className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
-          onClick={() => context.increment()}
+          onClick={(event) => {
+            event.stopPropagation();
+            context.increment();
+          }}
         >
           <PlusIcon className='h-6 w-6 text-black-500'></PlusIcon>
         </button>
