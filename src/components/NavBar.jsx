5420370da1b1759011c0b96a97828e890aec14be
MiltonCoronado <miltonext@hotmail.com>
2024-11-30 19:00:24 -0500

    style/heigth and backgrouncolor was modified

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -18,7 +18,7 @@ const NavBar = () => {
   const activeStyle = 'underline underline-offset-4';
 
   return (
-    <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light'>
+    <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-3.5 px-8 text-sm font-light bg-white'>
       <ul className='flex items-center gap-3'>
         <div className='font-semibold text-lg'>
             <NavElement
