2a38acff15d5872d17f77a0388f433e7898b60ca
MiltonCoronado <miltonext@hotmail.com>
2024-11-30 19:04:35 -0500

    feat/function fetchItems and state added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,35 @@
+import { useState, useEffect } from 'react';
+import Card from '../components/Card.jsx';
+
+
 const Home = () => {
+  const [products, setProducts] = useState([]); 
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const response = await fetch('https://fakestoreapi.com/products');
+      const data = await response.json();
+      setProducts(data);
+    }
+
+    fetchItems();
+  }, [])
+
+  console.log(products)
+
   return (
-    <div className='bg-red-100'>
-      Home
+    <div>
+      <p className='flex justify-center mb-8'>
+        Home
+      </p>
+      <div className='grid gap-x-4 gap-y-6 grid-cols-4 w-full max-w-screen-lg'>
+        {products.map(item => (
+          <Card 
+            key={item.id} 
+            data={item} 
+          />
+        ))}
+      </div>
     </div>
   )
 };
