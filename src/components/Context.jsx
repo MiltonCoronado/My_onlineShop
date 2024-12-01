3460ad5fbe650296bd2b3b5fcd0bde6180e73af8
MiltonCoronado <miltonext@hotmail.com>
2024-12-01 11:25:30 -0500

    function and state to handle producDetail added

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -3,17 +3,26 @@ import { createContext, useContext, useState } from 'react';
 const ShoppingCartContext = createContext();
 
 const ShoppingCartProvider = ({ children }) => {
-
   const [incrementProduct, setIncrementProduct] = useState(0);
-
   const increment = () => {
     setIncrementProduct(incrementProduct + 1);
   };
+
+  const [ProductDetailOpen, setProductDetailOpen] = useState(false);
+  const openSideMenu = () => setProductDetailOpen(true);
+  const closeSideMenu = () => setProductDetailOpen(false);
+
+  const [showProduct, setShowProduct] = useState(null);
 
   return (
     <ShoppingCartContext.Provider value={{
       incrementProduct,
       increment,
+      ProductDetailOpen,
+      openSideMenu,
+      closeSideMenu,
+      showProduct,
+      setShowProduct,
     }}>
       {children}
     </ShoppingCartContext.Provider>
