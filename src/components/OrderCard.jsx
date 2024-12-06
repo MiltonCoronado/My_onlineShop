6388403c8dc32c4813fd1ad09f8f924cd171d9ba
MiltonCoronado <miltonext@hotmail.com>
2024-12-06 18:15:11 -0500

    OrderCard component was created

---
+++ src/components/OrderCard.jsx
@@ -0,0 +1,20 @@
+import { XMarkIcon } from '@heroicons/react/24/solid';
+
+const OrderCard = ({ title, image, price }) => {
+  return (
+    <div className='flex justify-between items-center mb-3'>
+      <div className='flex items-center gap-2'>
+        <figure className='w-20 h-14'>
+          <img className='w-full h-full rounded-lg object-cover' src={image} alt={title}/>
+        </figure>
+        <p className='text-xs font-light'>{title}</p>
+      </div>
+      <div className='flex items-center gap-2'>
+        <p className='text-lg font-medium'>{price}</p>
+        <XMarkIcon className='h-6 w-6 text-black-500'></XMarkIcon>
+      </div>
+    </div>
+  )
+};
+
+export default OrderCard;
