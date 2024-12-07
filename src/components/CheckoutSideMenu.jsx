fe48d0a5490996de07af82749de2ad7e516a954d
MiltonCoronado <miltonext@hotmail.com>
2024-12-06 21:09:35 -0500

    style/overflow in y added

--- src/components/CheckoutSideMenu.jsx
+++ src/components/CheckoutSideMenu.jsx
@@ -8,7 +8,7 @@ const CheckoutSideMenu = () => {
   console.log('CheckoutSideMenu', context.showProduct)
 
   return (
-    <aside className={`${context.checkoutSideMenu ? 'flex' : 'hidden'} top-[63px] w-[360px] h-[calc(100vh-68px)] flex-col fixed right-0 border border-black rounded-lg bg-white z-10`}>
+    <aside className={`${context.checkoutSideMenu ? 'flex' : 'hidden'} top-[63px] w-[360px] h-[calc(100vh-68px)] flex-col fixed right-0 border border-black rounded-lg bg-white z-10 overflow-y-scroll`}>
       <div className='flex justify-between items-center p-6'>
         <h2 className='font-medium text-xl'>My Order</h2>
         <button>
