36add08c91a28bbd574df8985d1ab4f257cf4192
MiltonCoronado <miltonext@hotmail.com>
2024-12-08 22:05:37 -0500

    feat/render my last order with slice() and map()

--- src/pages/MyOrder.jsx
+++ src/pages/MyOrder.jsx
@@ -1,7 +1,25 @@
+import { MyContext } from '../components/Context';
+import OrderCard from '../components/OrderCard';
+
 const MyOrder = () => {
+  const context = MyContext();
+
+  console.log(context.order)
   return (
-    <div className='bg-red-100'>
-      MyOrder
+    <div>
+      <p className='flex justify-center mb-8'>
+        MyOrder
+      </p> 
+      <div className='flex flex-col w-96'>
+        {context.order.slice(-1)[0]?.products.map(item => (
+          <OrderCard 
+            key={item.id}
+            title={item.title}
+            image={item.image}
+            price={item.price}
+          />
+        ))}
+        </div>
     </div>
   )
 };
