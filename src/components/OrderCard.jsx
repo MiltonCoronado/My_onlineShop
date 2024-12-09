7343eeffeeba0dcb79c40b0d45c4ae9ccb7270eb
MiltonCoronado <miltonext@hotmail.com>
2024-12-08 22:02:01 -0500

    feat/function to see or not XMarkIcon was created

--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -1,6 +1,16 @@
 import { XMarkIcon } from '@heroicons/react/24/solid';
 
 const OrderCard = ({ deleteOrderCard, title, image, price }) => {
+  let renderXMarIcon;
+  
+  if (deleteOrderCard) {
+    renderXMarIcon = <XMarkIcon 
+    className='h-6 w-6 text-black-500 cursor-pointer'
+    onClick={() => deleteOrderCard()} 
+  >
+  </XMarkIcon>
+  }
+
   return (
     <div className='flex justify-between items-center mb-3'>
       <div className='flex items-center gap-2'>
@@ -10,12 +20,8 @@ const OrderCard = ({ deleteOrderCard, title, image, price }) => {
         <p className='text-xs font-light'>{title}</p>
       </div>
       <div className='flex items-center gap-2'>
-        <p className='text-lg font-medium'>{price}</p>
-        <XMarkIcon 
-          className='h-6 w-6 text-black-500 cursor-pointer'
-          onClick={() => deleteOrderCard()} 
-        >
-        </XMarkIcon>
+        <p className='text-lg font-medium ps-4'>{price}</p>
+        {renderXMarIcon}
       </div>
     </div>
   )
