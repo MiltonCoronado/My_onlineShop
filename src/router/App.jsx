05c01949aa36dcdaf00b1b622d0f5e034d9c8369
MiltonCoronado <miltonext@hotmail.com>
2024-12-08 22:06:39 -0500

    feat/new route to render page added

--- src/router/App.jsx
+++ src/router/App.jsx
@@ -22,6 +22,7 @@ const App = () => {
             <Route path='/my-account' element={<MyAccount />} />
             <Route path='/my-order' element={<MyOrder />} />
             <Route path='/my-orders' element={<MyOrders />} />
+            <Route path='/my-orders/last' element={<MyOrder />} />
             <Route path='/sign-in' element={<Signin />} />
             <Route path='*' element={<NotFound />} />
           </Routes>
