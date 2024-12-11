df04d1347a8803917fb9862e0256147d3fa219e8
MiltonCoronado <miltonext@hotmail.com>
2024-12-11 12:25:07 -0500

    feat/map to render card added

--- src/pages/MyOrders.jsx
+++ src/pages/MyOrders.jsx
@@ -1,7 +1,25 @@
+import { Link } from 'react-router-dom';
+import { MyContext } from '../components/Context';
+import OrdersCards from '../components/OrderCards';
+
 const MyOrders = () => {
+  const context = MyContext()
+
   return (
-    <div className='bg-red-100'>
-      MyOrders
+    <div>
+      <h1 className='flex justify-center'>
+        MyOrders
+      </h1>
+      {context.order.map((item, index) => (
+        <Link key={index} to={`/my-order`}>
+          <OrdersCards 
+            totalProducts={item.totalProducts}
+            totalPrice={item.totalPrice}
+          />
+        </Link>
+      ))}
+
+
     </div>
   )
 };
