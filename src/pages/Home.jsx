08fb3987777d68df45c596944934d244f11cd10e
MiltonCoronado <miltonext@hotmail.com>
2024-12-12 10:02:11 -0500

    refactor/feching data move to Context

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,9 @@
-import { useState, useEffect } from 'react';
+import { MyContext } from '../components/Context.jsx';
 import Card from '../components/Card.jsx';
 import ProductDetail from '../components/ProductDetail.jsx';
 
 const Home = () => {
-  const [products, setProducts] = useState([]); 
-
-  useEffect(() => {
-    const fetchItems = async () => {
-      const response = await fetch('https://fakestoreapi.com/products');
-      const data = await response.json();
-      setProducts(data);
-    }
-
-    fetchItems();
-  }, [])
+  const context = MyContext();
 
   return (
     <div>
@@ -21,7 +11,7 @@ const Home = () => {
         Home
       </p>
       <div className='grid gap-x-4 gap-y-6 grid-cols-4 w-full max-w-screen-lg'>
-        {products.map(item => (
+        {context.products.map(item => (
           <Card 
             key={item.id} 
             data={item} 
