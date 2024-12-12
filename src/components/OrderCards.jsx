a178de2c6d6a99e3a5a0674319e14978b49a66fe
MiltonCoronado <miltonext@hotmail.com>
2024-12-12 09:12:18 -0500

    feat/ChevronRightIcon component added

--- src/components/OrderCards.jsx
+++ src/components/OrderCards.jsx
@@ -1,13 +1,18 @@
-import { XMarkIcon } from '@heroicons/react/24/solid';
+import { ChevronRightIcon } from '@heroicons/react/24/solid';
 
 const OrdersCards = ({ totalPrice, totalProducts }) => {
-
   return (
-    <div className='flex justify-between items-center mb-3 border border-black'>
-      <p>
-        <span>01.02.25</span>
-        <span>{totalProducts}</span>
-        <span>{totalPrice}</span>
+    <div className='flex justify-between items-center border border-black w-80 p-4 rounded-lg mb-3'>
+      <p className='flex justify-between w-full'>
+        <span className='flex flex-col'>
+          <span className='font-light'>01.02.25</span>
+          <span className='font-light'>{totalProducts} Articles</span>
+        </span>
+      <p className='flex items-center gap-2'>
+        <span className='font-medium text-2xl'>${totalPrice}</span>
+        <ChevronRightIcon className='h-6 w-6 text-black cursor-pointer'>
+        </ChevronRightIcon>
+      </p>
       </p>
     </div>
   )
