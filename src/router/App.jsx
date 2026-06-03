41791333bd95385b345ba5bf5fda366c3b4dd82d
MiltonCoronado <miltonext@hotmail.com>
2026-06-03 15:55:27 -0500

    feat/setup dynamic routing with slug parameter

--- src/router/App.jsx
+++ src/router/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route} from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import MyAccount from '../pages/MyAccount';
 import MyOrder from '../pages/MyOrder';
@@ -7,28 +7,30 @@ import Signin from '../pages/Signin';
 import NotFound from '../pages/NotFound';
 import NavBar from '../components/NavBar';
 import Layout from '../components/Layout';
-import { ShoppingCartProvider } from '../components/Context.jsx';
 import CheckoutSideMenu from '../components/CheckoutSideMenu.jsx';
+import { ShoppingCartProvider } from '../components/Context.jsx';
 
 const App = () => {
   return (
     <HashRouter>
       <ShoppingCartProvider>
         <NavBar />
-        <Layout>
         <CheckoutSideMenu />
+        <Layout>
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/my-account' element={<MyAccount />} />
-            <Route path='/my-orders' element={<MyOrders />} />
-            <Route path='/my-order/:index' element={<MyOrder />} />
-            <Route path='/sign-in' element={<Signin />} />
-            <Route path='*' element={<NotFound />} />
+            {/* :slug => para capturar rutas dinamicas*/}
+            <Route path="/" element={<Home />} />
+            <Route path="/:slug" element={<Home />} />
+            <Route path="/my-account" element={<MyAccount />} />
+            <Route path="/my-orders" element={<MyOrders />} />
+            <Route path="/my-order/:index" element={<MyOrder />} />
+            <Route path="/sign-in" element={<Signin />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
       </ShoppingCartProvider>
     </HashRouter>
-  )
+  );
 };
 
 export default App;
