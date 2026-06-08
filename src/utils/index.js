66babf1d07ed403646876f601119e356a192e441
MiltonCoronado <miltonext@hotmail.com>
2026-06-08 15:07:34 -0500

    refactor/add JSDocs and validate if produtcs exists

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,11 @@
+/**
+ * Calcula el precio total de un array de productos
+ * @param {Array} products - Lista de productos con propiedad .price
+ * @returns {Number} - Total sumado y redondeado a 2 decimales
+ */
 export const totalPrice = (products) => {
-  return products.reduce((sum, product) => sum + product.price, 0)
-} 
+  if (!products || products.length === 0) return 0;
+  const sumTotal = products.reduce((sum, product) => sum + product.price, 0);
+
+  return Number(sumTotal.toFixed(2));
+};
