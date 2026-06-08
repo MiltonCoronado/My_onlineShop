5bfc4b7e981e99baa859547a30fa771a27c9b54a
MiltonCoronado <miltonext@hotmail.com>
2026-06-08 15:18:01 -0500

    style/changed class to better render image

--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -2,29 +2,34 @@ import { XMarkIcon } from '@heroicons/react/24/solid';
 
 const OrderCard = ({ deleteOrderCard, title, image, price }) => {
   let renderXMarIcon;
-  
+
   if (deleteOrderCard) {
-    renderXMarIcon = <XMarkIcon 
-    className='h-6 w-6 text-black-500 cursor-pointer'
-    onClick={() => deleteOrderCard()} 
-  >
-  </XMarkIcon>
+    renderXMarIcon = (
+      <XMarkIcon
+        className="h-6 w-6 text-black-500 cursor-pointer"
+        onClick={() => deleteOrderCard()}
+      ></XMarkIcon>
+    );
   }
 
   return (
-    <div className='flex justify-between items-center mb-3'>
-      <div className='flex items-center gap-2'>
-        <figure className='w-20 h-14'>
-          <img className='w-full h-full rounded-lg object-cover' src={image} alt={title}/>
+    <div className="flex justify-between items-center mb-3">
+      <div className="flex items-center gap-2">
+        <figure className="w-20 h-14">
+          <img
+            className="w-16 h-16 min-w-16 max-h-16 rounded-lg object-cover flex-shrink-0"
+            src={image}
+            alt={title}
+          />
         </figure>
-        <p className='text-xs font-light'>{title}</p>
+        <p className="text-xs font-light">{title}</p>
       </div>
-      <div className='flex items-center gap-2'>
-        <p className='text-lg font-medium ps-4'>{price}</p>
+      <div className="flex items-center gap-2">
+        <p className="text-lg font-medium ps-4">{price}</p>
         {renderXMarIcon}
       </div>
     </div>
-  )
+  );
 };
 
 export default OrderCard;
